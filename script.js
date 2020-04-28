cfe2733bf942ad22fac8cb2337e81c9c37352120
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-28 20:17:16 +0900

    shuffle list items

--- script.js
+++ script.js
@@ -19,11 +19,15 @@ let dragStartIndex;
 createList();
 
 function createList() {
-  richestPeople.forEach((person, index) => {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("data-index", index);
+  richestPeople
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
 
-    listItem.innerHTML = `
+      listItem.innerHTML = `
     <span class="number">${index + 1}</span>
     <div class="draggable" draggable="true">
       <p class="person-name">${person}</p>
@@ -31,7 +35,7 @@ function createList() {
     </div>
     `;
 
-    listItems.push(listItem);
-    draggable_list.appendChild(listItem);
-  });
+      listItems.push(listItem);
+      draggable_list.appendChild(listItem);
+    });
 }
