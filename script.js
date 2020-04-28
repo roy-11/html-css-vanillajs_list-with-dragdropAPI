f219ef6c20588d8255573629b7ff4d05977fa87a
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-28 21:34:31 +0900

    add check order logic

--- script.js
+++ script.js
@@ -94,3 +94,18 @@ function swapItems(fromIndex, toIndex) {
   listItems[fromIndex].appendChild(itemTwo);
   listItems[toIndex].appendChild(itemOne);
 }
+
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
+}
+
+check.addEventListener("click", checkOrder);
